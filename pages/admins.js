53e6c4d9a8c02b8caee1b2649543a86f6c4a7fcd
engineeredbyalex <nonstopzen26@gmail.com>
2024-01-17 19:50:23 +0200

    17.01 rev-3

--- pages/admins.js
+++ pages/admins.js
@@ -1,27 +1,110 @@
-import { mongooseConnect } from "@/lib/mongoose";
-import { isAdminRequest } from "@/pages/api/auth/[...nextauth]";
-import { Admin } from "@/models/Admin";
+import Layout from "@/components/Layout/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "@/components/Basics/Spinner";
+import { prettyDate } from "@/lib/date";
 
-export default async function handle(req, res) {
-  await mongooseConnect();
-  await isAdminRequest(req, res);
+function AdminsPage() {
+  const [email, setEmail] = useState('');
+  const [adminEmails, setAdminEmails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  if (req.method === 'POST') {
-    const { email } = req.body;
-    if (await Admin.findOne({ email })) {
-      res.status(400).json({ message: 'admin already exists!' });
-    } else {
-      res.json(await Admin.create({ email }));
+  function addAdmin(ev) {
+    ev.preventDefault();
+    axios.post('/api/admins', { email })
+      .then(res => {
+        console.log(res.data);
+        alert('Admin created!');
+        setEmail('');
+        loadAdmins();
+      })
+      .catch(err => {
+        alert(`Error!\n${err.response.data.message}`);
+      });
+  }
+
+  function deleteAdmin(_id, email) {
+    const confirmDelete = window.confirm(`Do you want to delete admin ${email}?`);
+
+    if (confirmDelete) {
+      axios.delete('/api/admins?_id=' + _id)
+        .then(() => {
+          alert('Admin deleted!');
+          loadAdmins();
+        });
     }
   }
 
-  if (req.method === 'DELETE') {
-    const { _id } = req.query;
-    await Admin.findByIdAndDelete(_id);
-    res.json(true);
+  function loadAdmins() {
+    setIsLoading(true);
+    axios.get('/api/admins')
+      .then(res => {
+        setAdminEmails(res.data);
+        setIsLoading(false);
+      });
   }
 
-  if (req.method === 'GET') {
-    res.json(await Admin.find());
-  }
+  useEffect(() => {
+    loadAdmins();
+  }, []);
+
+  return (
+    <Layout>
+      <h3 className="uppercase grey_text">Administratori</h3>
+      <h4 className="uppercase grey_text">Adaugă un administrator nou</h4>
+      <form onSubmit={addAdmin}>
+        <div className="flex gap-2 mt-10">
+          <input
+            type="text"
+            className="mb-0 w-[15rem]"
+            value={email}
+            onChange={ev => setEmail(ev.target.value)}
+            placeholder="Google Email" />
+          <button
+            type="submit"
+            className="btn-primary py-1 whitespace-nowrap rounded-lg">
+            Adaugă administrator
+          </button>
+        </div>
+      </form>
+      <h4 className="uppercase grey_text mt-10">Administratori existenți</h4>
+      <table className="basic ">
+        <thead>
+          <tr>
+            <th className="text-left">Adresă G-Mail administrator</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {isLoading && (
+            <tr>
+              <td colSpan={2}>
+                <div className="py-4">
+                  <Spinner fullWidth={true} />
+                </div>
+              </td>
+            </tr>
+          )}
+          {adminEmails.length > 0 && adminEmails.map(adminEmail => (
+            <tr key={adminEmail._id}>
+              <td>{adminEmail.email}</td>
+              <td>
+                {adminEmail.createdAt && prettyDate(adminEmail.createdAt)}
+              </td>
+              <td>
+                <button
+                  onClick={() => deleteAdmin(adminEmail._id, adminEmail.email)}
+                  className="btn-red">
+                  Șterge
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
 }
+
+export default AdminsPage;
